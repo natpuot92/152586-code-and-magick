c8b882581e7a424b33eecb0d213dc3769a9462ef
natpuot92 <natpuot1992@gmail.com>
2017-04-02 18:19:35 +0300

    ошибки тревиса, функция нахождения максимума, тернарный оператор

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.renderStatistics = function (ctx, names , times) {
+window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
@@ -13,40 +13,32 @@ window.renderStatistics = function (ctx, names , times) {
   ctx.fillText('Ура, вы победили!', 140, 35);
   ctx.strokeText('Список результатов:', 140, 55);
 
-  function getMaxNumber() {
-    var max = times[0];
-    for (var i = 0; i < times.length; i++) {
-      var time = times[i]
-      if (max < time) {
-        max = time;
+  function getMaxNumber(array) {
+    var max = array[0];
+    for (var i = 0; i < array.length; i++) {
+      var digit = array[i];
+      if (max < digit) {
+        max = digit;
       }
     }
     return max;
   }
 
-  var histogramHeight = 150;
-  var step = -150 / getMaxNumber();
-
-  for(var i = 0; i < times.length; i++) {
-    ctx.fillStyle = 'black';
-    ctx.fillText(parseInt(times[i]), 140 + i * 100, 230 + times[i] * step);
-    ctx.font = '16px PT Mono';
-    ctx.fillStyle = 'black';
-    ctx.fillText(names[i], 140 + i * 100, 260);
-  }
+  var step = -150 / getMaxNumber(times);
 
   function getRandomOpacity() {
     var randomOpacity = Math.random();
     var RGBArandomOpacity = randomOpacity.toFixed(1);
-    if (RGBArandomOpacity < 0.1) {
-      var RGBArandomOpacity = 0.1;
-      return RGBArandomOpacity;
-    } else {
-      return RGBArandomOpacity;
-    }
+    return RGBArandomOpacity < 0.1 ? 0.1 : RGBArandomOpacity;
   }
 
-  for(var i = 0; i < names.length; i++) {
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillStyle = 'black';
+    ctx.fillText(parseInt(times[i], 10), 140 + i * 100, 230 + times[i] * step);
+    ctx.font = '16px PT Mono';
+    ctx.fillStyle = 'black';
+    ctx.fillText(names[i], 140 + i * 100, 260);
+
     var name = names[i];
     if (name === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
@@ -56,5 +48,5 @@ window.renderStatistics = function (ctx, names , times) {
       ctx.fillRect(140 + 100 * i, 240, 40, times[i] * step);
     }
   }
-}
+};
 

345ab23b24157f09d0c5bb9bee1c539a9db3286e
natpuot92 <natpuot1992@gmail.com>
2017-05-25 22:25:18 +0300

    colorize

---
+++ js/colorize-element.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+
+  window.numberColor = 1;
+
+  window.colorizeElement = function(element, array, callback) {
+     element.addEventListener('click', function () {
+      if (window.numberColor < array.length) {
+        callback(element, array);
+        window.numberColor = window.numberColor + 1;
+    } else {
+        window.numberColor = 0;
+        callback(element, array);
+        window.numberColor = window.numberColor + 1;
+    };
+    });
+  }
+})();
--- js/wizzard-color.js
+++ js/wizzard-color.js
@@ -5,27 +5,17 @@
   var wizzardEyes = document.querySelector('.wizard-eyes');
   var wizzardFirball = document.querySelector('.setup-fireball-wrap');
 
-  window.numberColor = 1;
-  var changeColor = function (element, array) {
-    if (window.numberColor < array.length) {
-        element.style.fill = array[window.numberColor];
-        window.numberColor = window.numberColor + 1;
-    } else {
-        window.numberColor = 0;
-        element.style.fill = array[window.numberColor];
-        window.numberColor = window.numberColor + 1;
-    };
+  var howFill = function (element, arrayColor) {
+    element.style.fill = arrayColor[window.numberColor];
   }
 
-  wizzardCoat.addEventListener('click', function() {
-    changeColor(wizzardCoat, window.coatColorArray);
-  });
+  var howBackground = function (element, arrayColor) {
+   element.style.background = arrayColor[window.numberColor];
+  }
 
-  wizzardEyes.addEventListener('click', function() {
-    changeColor(wizzardEyes, window.eyesColorArray);
-  });
+  window.colorizeElement(wizzardCoat, window.coatColorArray, howFill);
+  window.colorizeElement(wizzardEyes, window.eyesColorArray, howFill);
+  window.colorizeElement(wizzardFirball, window.fireballColorArray, howBackground);
 
-  wizzardFirball.addEventListener('click', function() {
-    wizzardFirball.style.background = fireballColorArray[getRadomDigit(0, fireballColorArray.length)];
-  });
 })()
+

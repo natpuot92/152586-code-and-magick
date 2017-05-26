3af99e8c05d1a602dbe3d5d60a28e2c69eb23d84
natpuot92 <natpuot1992@gmail.com>
2017-05-27 00:40:40 +0300

    data

--- js/load.js
+++ js/load.js
@@ -8,28 +8,10 @@
     xhr.responseType = 'json';
     xhr.addEventListener('load', function (evt) {
       var wizzardData = xhr.response;
-      onload(wizzardData);
+      onLoad(wizzardData);
     })
 
     xhr.open('GET', url);
     xhr.send();
     }
-
-  var showSimilarWizzard = document.querySelector('.setup-similar');
-  var similarWizzardTemplate = document.getElementById('similar-wizard-template');
-  var similarWizzardShow = document.querySelector('.setup-similar-list');
-  var fragment = document.createDocumentFragment();
-
-  var onload = function (data) {
-    for (var wizzardIndex = 0; wizzardIndex < 4; wizzardIndex++) {
-      var wizzardElement = similarWizzardTemplate.content.cloneNode(true);
-      wizzardElement.querySelector('.setup-similar-label').textContent = data[wizzardIndex].name;
-      wizzardElement.querySelector('.wizard-coat').style.fill = data[wizzardIndex].colorCoat;
-      wizzardElement.querySelector('.wizard-eyes').style.fill = data[wizzardIndex].colorEyes;
-      fragment.appendChild(wizzardElement);
-    };
-    similarWizzardShow.appendChild(fragment);
-    showSimilarWizzard.classList.remove('hidden');
-  };
-
 })();
--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,25 @@
 'use strict';
 
 (function () {
-window.load('https://intensive-javascript-server-kjgvxfepjl.now.sh/code-and-magick/data', onload);
+
+  var showSimilarWizzard = document.querySelector('.setup-similar');
+  var similarWizzardTemplate = document.getElementById('similar-wizard-template');
+  var similarWizzardShow = document.querySelector('.setup-similar-list');
+  var fragment = document.createDocumentFragment();
+
+  var wizzards = function (data) {
+    for (var wizzardIndex = 0; wizzardIndex < 4; wizzardIndex++) {
+      var wizzardElement = similarWizzardTemplate.content.cloneNode(true);
+      wizzardElement.querySelector('.setup-similar-label').textContent = data[wizzardIndex].name;
+      wizzardElement.querySelector('.wizard-coat').style.fill = data[wizzardIndex].colorCoat;
+      wizzardElement.querySelector('.wizard-eyes').style.fill = data[wizzardIndex].colorEyes;
+      fragment.appendChild(wizzardElement);
+    };
+    similarWizzardShow.appendChild(fragment);
+    showSimilarWizzard.classList.remove('hidden');
+  };
+
+
+  window.load('https://intensive-javascript-server-kjgvxfepjl.now.sh/code-and-magick/data', wizzards);
 
 })();
